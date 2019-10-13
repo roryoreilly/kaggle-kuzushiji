53452ad4067c918b0a7fe4b02aeb1593aaeeeb1c
Rory O'Reilly <roryoreilly2@gmail.com>
2019-10-13 15:17:42 +0700

    Removed unnecessary data imports

--- fbmaskrcnn/maskrcnn_benchmark/data/datasets/__init__.py
+++ fbmaskrcnn/maskrcnn_benchmark/data/datasets/__init__.py
@@ -1,9 +1,11 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
 
-from .coco import COCODataset
-from .voc import PascalVOCDataset
-from .concat_dataset import ConcatDataset
+# from .coco import COCODataset
+# from .voc import PascalVOCDataset
+# from .concat_dataset import ConcatDataset
 from .abstract import AbstractDataset
 from .kuzushiji_dataset import KuzushijiDataset
 
-__all__ = ["COCODataset", "ConcatDataset", "PascalVOCDataset", "AbstractDataset", "KuzushijiDataset"]
+__all__ = [
+    # "COCODataset", "ConcatDataset", "PascalVOCDataset",
+           "AbstractDataset", "KuzushijiDataset"]
