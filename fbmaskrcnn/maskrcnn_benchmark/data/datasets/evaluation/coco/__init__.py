20db3549682105d28ef13b2e94bad2efba3b4198
Rory O'Reilly <roryoreilly2@gmail.com>
2019-10-13 15:36:20 +0700

    Removed CocoDataset import

--- fbmaskrcnn/maskrcnn_benchmark/data/datasets/evaluation/coco/__init__.py
+++ fbmaskrcnn/maskrcnn_benchmark/data/datasets/evaluation/coco/__init__.py
@@ -1,6 +1,6 @@
 from .coco_eval import do_coco_evaluation as do_orig_coco_evaluation
 from .coco_eval_wrapper import do_coco_evaluation as do_wrapped_coco_evaluation
-from maskrcnn_benchmark.data.datasets import AbstractDataset, COCODataset
+from maskrcnn_benchmark.data.datasets import AbstractDataset#, COCODataset
 
 
 def coco_evaluation(
@@ -12,17 +12,18 @@ def coco_evaluation(
     expected_results,
     expected_results_sigma_tol,
 ):
-    if isinstance(dataset, COCODataset):
-        return do_orig_coco_evaluation(
-            dataset=dataset,
-            predictions=predictions,
-            box_only=box_only,
-            output_folder=output_folder,
-            iou_types=iou_types,
-            expected_results=expected_results,
-            expected_results_sigma_tol=expected_results_sigma_tol,
-        )
-    elif isinstance(dataset, AbstractDataset):
+    # if isinstance(dataset, COCODataset):
+    #     return do_orig_coco_evaluation(
+    #         dataset=dataset,
+    #         predictions=predictions,
+    #         box_only=box_only,
+    #         output_folder=output_folder,
+    #         iou_types=iou_types,
+    #         expected_results=expected_results,
+    #         expected_results_sigma_tol=expected_results_sigma_tol,
+    #     )
+    # elif isinstance(dataset, AbstractDataset):
+    if isinstance(dataset, AbstractDataset):
         return do_wrapped_coco_evaluation(
             dataset=dataset,
             predictions=predictions,
